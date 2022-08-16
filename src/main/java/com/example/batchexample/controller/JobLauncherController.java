6fc12b6539112f6931b16cd73515be6c877b1bb2
jongyoon-park <cleanbook@naver.com>
2022-08-16 19:43:07 +0900

    feat: JobParameter 사용 예제 추가

---
+++ src/main/java/com/example/batchexample/controller/JobLauncherController.java
@@ -0,0 +1,38 @@
+package com.example.batchexample.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class JobLauncherController {
+
+    //단, 이런 식으로 배치 동작을 웹 서버에서 관리하는 것은 좋지 않음
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @GetMapping("/launchjob")
+    public String handle(@RequestParam("fileName") String fileName) throws Exception {
+
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addString("input.file.name", fileName)
+                    .addLong("time", System.currentTimeMillis())
+                    .toJobParameters();
+            jobLauncher.run(job, jobParameters);
+        } catch (Exception e) {
+            log.info(e.getMessage());
+        }
+
+        return "Done";
+    }
+}
